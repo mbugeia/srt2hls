51ac747c4c239ab4fa89fe547e66f8ce24e78d53
Maxime Bugeia <maxime.bugeia@gmail.com>
2019-09-28 22:20:47 +0200

    More generic python client

--- radio.py
+++ radio.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
 
 import argparse
+import sys
 import telnetlib
 
-parser = argparse.ArgumentParser()
-parser.add_argument("action", help="setlivesource <source>")
-parser.add_argument("livesource", nargs='?', default="", help="livesource to set")
+def remove_last_line_from_string(s):
+    return s[:s.rfind('\n')]
 
-args = parser.parse_args()
+def send_tn_command(command, parameters=""):
+    try:
+      tn = telnetlib.Telnet(host="127.0.0.1", port=8500)
+    except:
+      sys.exit("Failed to connect to telnet server, is liquidsoap running ?")
 
-tn = telnetlib.Telnet(host="127.0.0.1", port=8500)
+    tn.write((command + " " + parameters + "\n").encode('ascii'))
+    tn.write(b"exit\n")
 
-if args.action == "setlivesource":
-  if args.livesource != "":
-    tn.write(("setlivesource " + str(args.livesource) + "\n").encode('ascii'))
-  else:
-    print("Error: you need to provide a livesource")
+    return remove_last_line_from_string(tn.read_until(b"\nEND").decode('ascii'))
 
-if args.action == "getlivesource":
-  tn.write(b"getlivesource\n")
 
-tn.write(b"exit\n")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", help="getlivesource, setlivesource <source>")
+    parser.add_argument("parameters", nargs='?', default="", help="action parameters")
 
-print(tn.read_some().decode('ascii'))
+    args = parser.parse_args()
+
+    if args.command == "setlivesource" and args.parameters == "":
+        sys.exit("Error: you need to provide a livesource to switch to")
+    else:
+        print(send_tn_command(args.command, args.parameters))
+
